812f861f2aed1d560b060fcdcae97b9d9bd529d8
saniho <sanihogithub@gmail.com>
2024-01-01 20:54:42 +0100

    migration to new authentification requered by NetAtmo

--- custom_components/apiNetatmo/const.py
+++ custom_components/apiNetatmo/const.py
@@ -1,6 +1,6 @@
 """ Constants """
 
-__VERSION__ = "1.1.0.2" # attention updater aussi manifest.json
+__VERSION__ = "1.1.1.0" # attention updater aussi manifest.json
 
 
 CONF_REFRESH_TOKEN = "refresh_token"
